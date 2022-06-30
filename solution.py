37d650f0afaac6be69afa8e56ea00d97f5db95d9
Ally <65431232+armcgill@users.noreply.github.com>
2022-06-30 19:32:29 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,4 +3,14 @@ for i in range(1,9834):
         print(str((627 ** i) % 9834))
         exit()
 
+# Prints out solution: 5115
+
+
+#Additional Information
+#Public Mod: 9834
+#Public Base: 825
+#A Secret: 4191
+#B Secret: 2805
+#A Public: 5841
+#B Public: 627
 
